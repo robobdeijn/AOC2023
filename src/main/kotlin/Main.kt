8f7a8497bbf8bea3d96e11f8db71b1715fddad44
robobdeijn <rob.obdeijn@inergy.nl>
2023-12-01 12:59:21 +0100

    blah

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,7 +3,7 @@ import java.io.File
 val wordToDigit = mapOf("one" to '1', "two" to '2', "three" to '3', "four" to '4', "five" to '5',"six" to '6',"seven" to '7', "eight" to '8', "nine" to '9')
 
 fun main() {
-    val sum = File("/Users/robobdeijn/IdeaProjects/Aoc2023Kotlin/src/main/resources/day1.txt").useLines { it -> it.map {convertToDigit(it) }.sum() }
+    val sum = File("src/main/resources/day1.txt").useLines { it -> it.map {convertToDigit(it) }.sum() }
     println("sum: $sum")
 }
 
