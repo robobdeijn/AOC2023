5b33787a0bcacca8c67dd40cd2ebcdcf23e652c9
robobdeijn <rob.obdeijn@inergy.nl>
2023-12-03 09:58:35 +0100

    day3

--- src/main/kotlin/Day3.kt
+++ src/main/kotlin/Day3.kt
@@ -10,42 +10,26 @@ fun main() {
     println("Gear Ratio: $gearRatio")
 }
 
-class Schematic(val parts: List<PartNumber>, val symbols: List<Symbol>) {
+class Schematic(private val parts: List<PartNumber>, private val symbols: List<Symbol>) {
     companion object Factory {
-        fun create(schematicString: List<String>) : Schematic {
-            val partNumbers = schematicString.flatMapIndexed() { y, s -> extractPartNumbers(y, s) }
-            val syms = schematicString.flatMapIndexed() { y, s -> extractSymbols(y, s) }
-            return Schematic(partNumbers, syms);
-        }
+        fun create(schematicString: List<String>) : Schematic = Schematic(schematicString.flatMapIndexed { y, s -> extractPartNumbers(y, s) }, schematicString.flatMapIndexed { y, s -> extractSymbols(y, s) });
 
-        private fun extractSymbols(y: Int, line: String) : List<Symbol> {
-           return line.mapIndexedNotNull { x, c -> if (isSymbol(c)) Symbol(c, Coordinate(x, y)) else null }
-        }
-
+        private fun extractSymbols(y: Int, line: String) : List<Symbol> = line.mapIndexedNotNull { x, c -> if (isSymbol(c)) Symbol(c, Coordinate(x, y)) else null }
         private fun isSymbol(c: Char) = !(c.isDigit() || c == '.')
-
-        private fun extractPartNumbers(y: Int, line: String) : List<PartNumber> {
-            return Regex("\\d+").findAll(line).map { PartNumber(it.value.toInt(), Coordinate(it.range.first, y)) }.toList()
-        }
+        private fun extractPartNumbers(y: Int, line: String) : List<PartNumber> = Regex("\\d+").findAll(line).map { PartNumber(it.value.toInt(), Coordinate(it.range.first, y)) }.toList()
     }
 
-    fun gearRatio() : Int {
-        return symbols.filter { it.value == '*' }
+    fun gearRatio() : Int = symbols.filter { it.value == '*' }
             .map { s -> parts.filter { it.isAdjacent(s.coordinate) } }
             .filter { it.count() == 2 }
             .sumOf { it[0].number * it[1].number }
-    }
 
-    fun partNumberSum() : Int {
-        return parts.filter { p -> symbols.any { p.isAdjacent(it.coordinate) } }.sumOf { it.number }
-    }
+    fun partNumberSum() : Int = parts.filter { p -> symbols.any { p.isAdjacent(it.coordinate) } }.sumOf { it.number }
 }
 
-class PartNumber(val number: Int, val start: Coordinate) {
-    fun isAdjacent(coordinate: Coordinate) : Boolean {
-        return coordinate.x >= start.x -1 && coordinate.x <= (start.x + log10(abs(number.toDouble())).toInt() +1)
-                && coordinate.y >= start.y -1 && coordinate.y <= start.y + 1;
-    }
+class PartNumber(val number: Int, private val start: Coordinate) {
+    fun isAdjacent(coordinate: Coordinate) : Boolean = coordinate.x >= start.x -1 && coordinate.x <= (start.x + log10(abs(number.toDouble())).toInt() +1)
+                && coordinate.y >= start.y -1 && coordinate.y <= start.y + 1
 }
 
 data class Symbol(val value: Char, val coordinate: Coordinate)
